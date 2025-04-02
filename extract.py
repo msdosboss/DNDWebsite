064295a126363a2efdb0da95516769b9a3b9d6ea
msdosboss <jacobt0722@gmail.com>
2025-04-02 14:08:13 -0700

    extracted item data from excel and put in json

---
+++ extract.py
@@ -0,0 +1,54 @@
+from openpyxl import load_workbook
+
+def rowLen(sheet, column):
+	for rowIndex in range(sheet.max_row, 0, -1):
+		if sheet.cell(row = rowIndex, column = column).value != None:
+			return rowIndex
+
+def writeToJson(fileName, jsonEntries):
+	with open(fileName, "w") as f:
+		for jsonEntry in jsonEntries:
+			f.write(jsonEntry)
+			f.write("\n")
+
+if __name__ == "__main__":
+
+	wb = load_workbook(filename = 'magic_items_full.xlsx')
+
+	columnNames = []
+
+	sheet = wb["Updated 19Jan2020"]
+
+	for columnIndex in range(sheet.max_column):
+		columnNames.append(sheet.cell(row = 1, column = columnIndex + 1).value)
+
+	jsonEntries = [""] * (sheet.max_row + 1)
+	jsonEntries[0] = "["
+	jsonEntries[len(jsonEntries) - 1] = "]"
+
+	for j in range(1, sheet.max_row):
+		jsonEntries[j] = jsonEntries[j] + "  {"
+	
+	for i, columnName in enumerate(columnNames):
+		rowLength = rowLen(sheet, i + 1)
+
+		for j in range(1, sheet.max_row):
+			jsonEntries[j] = jsonEntries[j] + f" \"{sheet.cell(row = 1, column = i + 1).value}\": "
+
+			if isinstance(sheet.cell(row = j + 1, column = i + 1).value, str):
+				value = sheet.cell(row = j + 1, column = i + 1).value.replace("\"","\\\"")
+				jsonEntries[j] = jsonEntries[j] + f"\"{value}\","
+			
+			elif sheet.cell(row = j + 1, column = i + 1).value is None:
+				jsonEntries[j] = jsonEntries[j] + "null,"
+			
+			else:
+				jsonEntries[j] = jsonEntries[j] + f"{sheet.cell(row = j + 1, column = i + 1).value},"
+
+	for j in range(1, sheet.max_row):
+		jsonEntries[j] = jsonEntries[j][:-1]	#gets rid of last , for the key pair values
+		jsonEntries[j] = jsonEntries[j] + " },"
+
+	jsonEntries[len(jsonEntries) - 2] = jsonEntries[len(jsonEntries) - 2][:-1]	#gets rid of last , for the }
+
+	writeToJson("items.json", jsonEntries)	
---
+++ index.js
@@ -0,0 +1,5 @@
+fetch("items.json")
+  .then(response => response.json())
+  .then(data => console.log(data[0])) // Use the data
+  .catch(error => console.error("Error loading database:", error));
+
